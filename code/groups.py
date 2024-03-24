75b8fe31bb5f1c84187912c09b5b6d9cb44b00d4
Quynh Dang <quynhdtd3502@gmail.com>
2024-03-24 16:37:56 +0700

    add camera

---
+++ code/groups.py
@@ -0,0 +1,15 @@
+from settings import *
+
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surf = pygame.display.get_surface()
+        self.offset = vector()
+        
+    def draw(self, target_pos):
+        self.offset.x = -(target_pos[0] - WINDOW_WIDTH/2)
+        self.offset.y = -(target_pos[1] - WINDOW_HEIGHT/2)
+        
+        for sprite in self:
+            offset_pos = sprite.rect.topleft + self.offset
+            self.display_surf.blit(sprite.image, offset_pos)
--- code/level.py
+++ code/level.py
@@ -1,13 +1,14 @@
 from settings import *
 from sprites import Sprite, MovingSprite
 from player import Player
+from groups import AllSprites
 
 class Level:
     def __init__(self, tmx_map):
         self.display_surf = pygame.display.get_surface()
         
         #groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
         self.semi_colli_sprites = pygame.sprite.Group()
         
@@ -21,7 +22,7 @@ class Level:
         # objects    
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'player':
-                Player((obj.x, obj.y), self.all_sprites, self.collision_sprites, self.semi_colli_sprites)
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites, self.semi_colli_sprites)
         
         # moving objects
         for obj in tmx_map.get_layer_by_name('Moving Objects'):
@@ -43,4 +44,4 @@ class Level:
     def run(self, dt):
         self.all_sprites.update(dt)
         self.display_surf.fill('black')
-        self.all_sprites.draw(self.display_surf)
+        self.all_sprites.draw(self.player.hitbox_rect.center)
