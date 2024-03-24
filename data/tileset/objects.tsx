8fc76b03c688546a451d33495bc5229a575ae0d7
Quynh Dang <quynhdtd3502@gmail.com>
2024-03-24 10:42:50 +0700

    basic jump and collision

---
+++ data/tileset/objects.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="134" tileheight="116" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image width="134" height="116" source="C:/Users/dtdqu/OneDrive/Desktop/Sprite/ss-steve.png"/>
+ </tile>
+</tileset>
