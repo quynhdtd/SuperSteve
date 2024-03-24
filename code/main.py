fe64fc6347c42c2b8868298b682acc12a1897058
quynhdtd <98536392+quynhdtd@users.noreply.github.com>
2024-03-24 10:54:45 +0700

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -1,22 +1,28 @@
 from settings import *
 from level import Level
+from pytmx.util_pygame import load_pygame
+from os.path import join
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surf = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Super Steve')
+        self.clock = pygame.time.Clock()
         
-        self.current_stage = Level()
+        self.tmx_maps = {0: load_pygame(join('.', 'data', 'levels', 'omni.tmx'))}        
+        self.current_stage = Level(self.tmx_maps[0])
+
         
     def run(self):
         while True:
+            delta_time = self.clock.tick() / 1000
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
             
-            self.current_stage.run()
+            self.current_stage.run(delta_time)
                     
             pygame.display.update()
 
